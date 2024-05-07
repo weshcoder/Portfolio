fb9c57303f02714d45eb7321646734932637b67e
weshcoder <wegairungum21@gmail.com>
2024-05-07 21:50:17 +0300

    Update

--- assets/js/johndoe.js
+++ assets/js/johndoe.js
@@ -43,6 +43,7 @@ $(window).on("load", function() {
         }
     }), $(".filters a").click(function() {
         $(".filters .active").removeClass("active"), $(this).addClass("active");
+        $(".filters .activeAccomplishments").removeClass(".activeAccomplishments");
         var i = $(this).attr("data-filter");
         return t.isotope({
             filter: i,
